d8478caf568d598b08aeb850a59908935a056081
Egor Marchenko <t469mo@gmail.com>
2021-11-02 17:26:00 +0300

    springCRUD_CREATE_READ_UPDATE_DELETE

--- src/main/java/ru/egorkuban/springcrud/dao/PersonDAO.java
+++ src/main/java/ru/egorkuban/springcrud/dao/PersonDAO.java
@@ -5,6 +5,7 @@ import ru.egorkuban.springcrud.model.Person;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT = 1;
@@ -19,17 +20,29 @@ public class PersonDAO {
         people.add(new Person(++PEOPLE_COUNT, "Lewis"));
     }
 
-    public List<Person> index(){
+    public List<Person> index() {
         return people;
     }
-    public Person show(int id){
+
+    public Person show(int id) {
         return people.stream()
-                .filter(people->people.getId() == id)
+                .filter(people -> people.getId() == id)
                 .findAny()
                 .orElse(null);
     }
-    public void save(Person person){
+
+    public void save(Person person) {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
+    }
+
+    public void update(int id, Person updatePerson) {
+        Person personToBeUpdate = show(id);
+        personToBeUpdate.setName(updatePerson.getName());
+
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
     }
 }
